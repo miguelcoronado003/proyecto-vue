5eb09745a692ad63deb7e672d7e13f257682702a
miguelcoronado003 <montanomiguel2013@gmail.com>
2024-11-26 13:25:03 -0500

    revisar si aca es el commit

---
+++ src/components/SideBar.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="sidebar">
+        <h2> barra  lateral</h2>
+        <router-link to="/listaDeTareas">lista de tareas</router-link>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.sidebar{
+    width: 200px;
+    background-color: #9c9ea1;
+    padding: 20px;
+}
+</style>
